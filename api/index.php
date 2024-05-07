35051b2d064e9dd4329a058c216f38370fe1f271
Matheexx <89420055+Matheexx@users.noreply.github.com>
2024-05-07 14:54:24 -0300

    Teste com o código principal e conexão com db

--- api/index.php
+++ api/index.php
@@ -2,9 +2,9 @@
 
 try {
     $connection = mysqli_connect("3ht.h.filess.io", "teste_diecoffee", "f1fe7f0e3c02444f1eae39a07aecac5d52b4b828", "teste_diecoffee", 3307);
-    // $users = $connection->query("SELECT * FROM users");
-    // foreach ($users as $user) {
-        /* echo
+    $users = $connection->query("SELECT * FROM users");
+    foreach ($users as $user) {
+        echo
         "
         <div style='background-color: #d0d0d0; width: 200px; padding: 5px; border-radius: 7px; border: 1px solid black;'>
             <p>id: {$user['id']}</p>
@@ -13,8 +13,7 @@ try {
             <p>senha: {$user['password']}</p>
         </div>
         ";
-    
-    } */
+    }
 } catch (\Throwable $e) {
     echo $e;
 }
